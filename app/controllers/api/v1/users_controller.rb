ab6d2889515442cf28d5d495bb519a417ff43506
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-15 14:41:13 -0600

    feat: add login endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,20 +3,24 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     if params[:password] == params[:password_confirmation]
-      # binding.pry
-      if user = User.find_by!(email: params[:email])
+      user = User.create!(email: params[:email], password: params[:password])
+      user.api_key = SecureRandom.hex(15)
+      if user.save
         render json: UserSerializer.new_user(user), status: 201
-      elsif
-        user = User.create!(email: params[:email], password: params[:password])
-
-        if user.save
-          render json: UserSerializer.new_user(user), status: 201
-        end
-      end 
+      end
     elsif params[:password] != params[:password_confirmation]
       render json: ErrorSerializer.password_mismatch, status: 422
     end  
   end 
+
+  def login
+    user = User.find_by(email: user_params[:email])
+    if user && user.authenticate(user_params[:password])
+      render json: UserSerializer.session_login(user)
+    else
+      render json: ErrorSerializer.bad_credentials, status: 422
+    end
+  end
 
   
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
 
       post '/users', to: 'users#create'
+      get '/users', to: 'users#login'
     
     end 
   end 
