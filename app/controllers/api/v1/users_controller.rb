d9a4858c0fe6e77fef26c17e1b3300bb63ec5ece
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-15 14:20:18 -0600

    feat: chaznge user create action to find or create by email

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,8 +3,7 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     if params[:password] == params[:password_confirmation]
-      user = User.create!(email: params[:email], password: params[:password])
-      user.api_key = SecureRandom.hex(15)
+      user = User.find_or_create_by!(email: params[:email])
       if user.save
         render json: UserSerializer.new_user(user), status: 201
       end
