376e77c274d9d634a1e8e1c11dfc863b9bb4fb25
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-15 14:28:45 -0600

    fix bug causing unable to find user by email bug

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,10 +3,16 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     if params[:password] == params[:password_confirmation]
-      user = User.find_or_create_by!(email: params[:email])
-      if user.save
+      # binding.pry
+      if user = User.find_by!(email: params[:email])
         render json: UserSerializer.new_user(user), status: 201
-      end
+      elsif
+        user = User.create!(email: params[:email], password: params[:password])
+
+        if user.save
+          render json: UserSerializer.new_user(user), status: 201
+        end
+      end 
     elsif params[:password] != params[:password_confirmation]
       render json: ErrorSerializer.password_mismatch, status: 422
     end  
